01a3c37c00c1e0bd16a40b5fca315508b23b13e1
jamy123456 <990134943@qq.com>
2019-05-06 23:27:57 +0800

    1234

--- demo1/src/demo1/StudyScore.java
+++ demo1/src/demo1/StudyScore.java
@@ -2,16 +2,17 @@ package demo1;
 
 public class StudyScore {
 	public static void main(String[] args) {
+		// 1 2 3
 		int score = 85;
-		if(score < 60) {
+		if (score < 60) {
 			System.out.println("该学生成绩不及格");
-		}else if(score <= 69) {
+		} else if (score <= 69) {
 			System.out.println("该学生成绩及格");
-		}else if(score <= 79 && score >=70) {
+		} else if (score <= 79 && score >= 70) {
 			System.out.println("该学生成绩一般");
-		}else if(score <= 89 && score >=80 ) {
+		} else if (score <= 89 && score >= 80) {
 			System.out.println("该学生成绩良好");
-		}else if(score <=100 && score >=90) {
+		} else if (score <= 100 && score >= 90) {
 			System.out.println("该学生成绩优秀");
 		}
 	}
