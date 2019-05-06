13c8b76b109e0aa696038d2b54372bab4e802621
jamy123456 <990134943@qq.com>
2019-05-06 23:29:03 +0800

    //

--- demo1/src/demo1/StudyScore.java
+++ demo1/src/demo1/StudyScore.java
@@ -4,6 +4,7 @@ public class StudyScore {
 	public static void main(String[] args) {
 		// 1 2 3
 		int score = 85;
+		//
 		if (score < 60) {
 			System.out.println("该学生成绩不及格");
 		} else if (score <= 69) {
