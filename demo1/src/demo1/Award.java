228031859e17884515446f53ebdfa1a36abdb605
jamy123456 <990134943@qq.com>
2019-07-06 14:08:33 +0800

    删除6

--- demo1/src/demo1/Award.java
+++ demo1/src/demo1/Award.java
@@ -3,7 +3,7 @@ package demo1;
 public class Award {
     //123450000000000000000000000000000
 	public static void main(String[] args) {
-		//23456
+		//2345
 		int yearNum = 7;
 		if (yearNum >= 8) {
 			System.out.println("年终奖为30000");
