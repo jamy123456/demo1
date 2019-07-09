65f670171eb25d25317c24315c6a994afc46aaf5
jamy123456 <39671040+jamy123456@users.noreply.github.com>
2019-07-09 22:56:34 +0800

    添加别人的修改

--- demo1/src/demo1/Award.java
+++ demo1/src/demo1/Award.java
@@ -11,7 +11,7 @@ public class Award {
 		}else if (yearNum >= 5) {
 			System.out.println("年终奖为10000");
 		}else if (yearNum >= 3) {
-			 //添加别人的修改
+			 //添加别人的修改000000000000000000000000000000000000000000000000000000
 			System.out.println("年终奖为5000");
 		}else if (yearNum >= 1) {
 			System.out.println("年终奖为1000");
