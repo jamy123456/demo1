9e11f1808e52c1bc6a7734bdcf9a8d2a0a4c09c2
jamy123456 <990134943@qq.com>
2019-07-09 22:19:44 +0800

    1111111111111111111111

--- demo1/src/demo1/Award.java
+++ demo1/src/demo1/Award.java
@@ -5,12 +5,12 @@ public class Award {
 	public static void main(String[] args) {		
 		int yearNum = 7;
 		if (yearNum >= 8) {
+			//添加自己的修改
 			//00000000000000000000000000000000000000
 			System.out.println("年终奖为30000");
 		}else if (yearNum >= 5) {
 			System.out.println("年终奖为10000");
 		}else if (yearNum >= 3) {
-			 //别人的修改
 			System.out.println("年终奖为5000");
 		}else if (yearNum >= 1) {
 			System.out.println("年终奖为1000");
