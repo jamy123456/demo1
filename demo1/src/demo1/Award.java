3eb1743bf5f77309d896069754421c5fccff4aae
jamy123456 <990134943@qq.com>
2019-07-09 22:30:37 +0800

    1钱钱钱1111111111111

--- demo1/src/demo1/Award.java
+++ demo1/src/demo1/Award.java
@@ -5,13 +5,13 @@ public class Award {
 	public static void main(String[] args) {		
 		int yearNum = 7;
 		if (yearNum >= 8) {
-			//添加自己的修改
+			//添加自己的修改111111111111111111111111
 			//00000000000000000000000000000000000000
 			System.out.println("年终奖为30000");
 		}else if (yearNum >= 5) {
 			System.out.println("年终奖为10000");
 		}else if (yearNum >= 3) {
-			 //添加别人的修改
+			 //添加别人的修改11111111111111111111111111111111111111111
 			System.out.println("年终奖为5000");
 		}else if (yearNum >= 1) {
 			System.out.println("年终奖为1000");
