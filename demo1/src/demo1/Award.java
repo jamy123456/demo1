2940e0d90959e5ac8fa59b99825d6f625dcb1314
jamy123456 <990134943@qq.com>
2019-07-10 20:55:07 +0800

    添加自己的更新999999999999999999999999999999

--- demo1/src/demo1/Award.java
+++ demo1/src/demo1/Award.java
@@ -1,7 +1,8 @@
 package demo1;
 
 public class Award {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		//添加自己的更新999999999999999999999999999999999999
 		int yearNum = 7;
 		if (yearNum >= 8) {
 			System.out.println("年终奖为30000");
