76d55948789d7dd86d8ee1d3fbcbe048268a7f3e
jamy123456 <990134943@qq.com>
2019-07-31 16:11:56 +0800

    代理模式添加注释

--- demo1/src/proxy/Proxy.java
+++ demo1/src/proxy/Proxy.java
@@ -3,6 +3,7 @@ package proxy;
 //   代理类
 public class Proxy implements Subject{
     private RealSubject realSubject;
+    // 不影响被代理类的情况下,实现了被代理类方法的集成和代理类的方法扩展,这是基于代理类和被代理类都必须实现共同的接口
     @Override
     public void request() {
         if (realSubject == null){
