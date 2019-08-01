a3de658ca8cbfda602d3c61df9198af9b4249c5f
jamy123456 <990134943@qq.com>
2019-08-01 14:57:35 +0800

    外观模式案例添加

---
+++ demo1/src/facade/FacadeTest.java
@@ -0,0 +1,9 @@
+package facade;
+
+// 外环模式测试
+public class FacadeTest {
+	public static void main(String[] args) {
+		Facade facade = new Facade();
+		facade.method();
+	}
+}
---
+++ demo1/src/facade/SubSystem3.java
@@ -0,0 +1,8 @@
+package facade;
+
+public class SubSystem3 {
+	
+	public  void method3() {
+		System.out.println("子系统3的method3方法被调用!.............");
+	}
+}
