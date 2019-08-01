ebc1ba7c25a709df7b29373232046fa6b72ca09c
jamy123456 <990134943@qq.com>
2019-08-01 15:57:50 +0800

    状态模式案例添加

---
+++ demo1/src/state/ConcreteStateA.java
@@ -0,0 +1,9 @@
+package state;
+
+public class ConcreteStateA extends State{
+    @Override
+    public void handle(Context context) {
+        System.out.println("当前状态是A");
+        context.setState(new ConcreteStateB());
+    }
+}
