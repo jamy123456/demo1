8f126a07dae751df27fbf3ce6b6bd8c6506387ef
jamy123456 <990134943@qq.com>
2019-08-13 10:42:06 +0800

    cglib动态代理案例

---
+++ demo1/src/cglibproxy/TeacterDao.java
@@ -0,0 +1,11 @@
+package cglibproxy;
+
+/**
+ * Created by jamy on 2019-08-12
+ */
+public class TeacterDao {
+
+    public void teach() {
+        System.out.println(" 老师授课中,我是cglib代理,不需要实现接口!!!");
+    }
+}
