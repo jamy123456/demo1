5d04e7c62be04999fedb30bc4f1da1f27efaf662
jamy123456 <990134943@qq.com>
2019-08-20 16:56:16 +0800

    冒泡排序demo优化

--- demo1/src/demo1/BubbleSortDemo.java
+++ demo1/src/demo1/BubbleSortDemo.java
@@ -15,13 +15,18 @@ public class BubbleSortDemo {
     // 冒泡排序
     public static void sort(int[] arr) {
         int temp;
+        boolean flag = true;// 加入这个flag开关,当无需排序时,走完第一局就直接跳出循环,提高效率
         for (int i=0; i<arr.length-1; i++) {
             for (int j=0; j<arr.length-1-i; j++) {
                 if (arr[j] > arr[j+1]) {
                     temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = false;
                 }
+            }
+            if (flag == true) {
+                break;
             }
         }
     }
